bfcb356f194ccde2741246b2aa2940d066ecf078
sumitchitlange <chitlange.sumit@gmail.com>
2025-03-06 20:28:59 +0530

    updated code

--- config/settings.py
+++ config/settings.py
@@ -86,20 +86,3 @@ showAiErrorAlerts = True            # True or False, Note: True or False are cas
 
 
 
-
-############################################################################################################
-'''
-THANK YOU for using my tool 😊! Wishing you the best in your job hunt 🙌🏻!
-
-Sharing is caring! If you found this tool helpful, please share it with your peers 🥺. Your support keeps this project alive.
-
-Support my work on <PATREON_LINK>. Together, we can help more job seekers.
-
-As an independent developer, I pour my heart and soul into creating tools like this, driven by the genuine desire to make a positive impact.
-
-Your support, whether through donations big or small or simply spreading the word, means the world to me and helps keep this project alive and thriving.
-
-Gratefully yours 🙏🏻,
-Sai Vignesh Golla
-'''
-############################################################################################################
